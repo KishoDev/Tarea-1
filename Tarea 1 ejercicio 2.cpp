c38ebb7e62b726b8c4827b9226744754719f0a6d
Kisho_dev <114254057+KishoDev@users.noreply.github.com>
2023-04-19 10:49:36 -0400

    Se arregló básicamente casi todo el código.

--- Tarea 1 ejercicio 2.cpp
+++ Tarea 1 ejercicio 2.cpp
@@ -13,7 +13,7 @@ struct Persona{
     bool quiere_intercambiar;
 };
 
-int l;  //Una variable global para guardar el numero de personas a ingresar
+int numPersonas;  //Una variable global para guardar el numero de personas a ingresar
 
 /*---------------------------------------------------------------------------------------*/
 
@@ -21,30 +21,33 @@ int l;  //Una variable global para guardar el numero de personas a ingresar
 * int* comprarTarjeta
 ******
 * Resumen:
-* 	Crea las un arreglo de enteros para las personas, estos arreglos serán sus tarjetas
+*   Crea las un arreglo de enteros para las personas, estos arreglos serán sus tarjetas
 ******
 * Input:
-* 	-string nombre; es el nombre de la persona
-*   -int dia; es el numero de dia en que se encuentran 	
+*   -string nombre; es el nombre de la persona
+*   -int dia; es el numero de dia en que se encuentran  
 *   -int &m; es el tamaño del arreglo 
 *   -int* arr; es el arreglo de enteros en donde se guardara para luego ser retornado
 ******
 * Returns:
-* 	Retrona arr; que es el arreglo de enteros que contiene la tarjeta de la persona
+*   Retrona arr; que es el arreglo de enteros que contiene la tarjeta de la persona
 *****/
 
 int* comprarTarjeta(string nombre, int dia, int &m){
+    m = nombre.length();
     int* arr = new int[m]; 
-    cout<<"Nombre "<<nombre<<endl;
+    /*
+    cout << "Nombre " << nombre <<endl;
 
-    cout<<"tamanio "<<m<< endl ;
+    cout<<"tamanio " << m << endl ;
     cout<<"[";
-    for(int j=0; j<m; j++){           
+    */
+    for (int j = 0; j < m; j++){           
         arr[j] = nombre[j] % dia ;
-        cout<<arr[j]<<",";
+        //cout << arr[j] << ",";
         }
-    cout<<"]\n";
-        return arr;
+    //cout<<"]\n";
+    return arr;
 
 };
 
@@ -53,7 +56,7 @@ int* comprarTarjeta(string nombre, int dia, int &m){
 * bool elemento_comun
 ******
 * Resumen:
-* 	Comprueba si en las tarjetas (arreglo de enteros) de dos personas, existe un elemento en comun.
+*   Comprueba si en las tarjetas (arreglo de enteros) de dos personas, existe un elemento en comun.
 ******
 * Input:
 *   -Persona* p1; es un arreglo de struct en donde se guarda los datos de una persona 
@@ -61,14 +64,14 @@ int* comprarTarjeta(string nombre, int dia, int &m){
 *   -int *aux; es un arreglo en donde se almacenara temporalmente la tarjeta de p1 (Ayuda a hacer el intercambio más facil)
 ******
 * Returns:
-* 	No retorna nada ya que es una funcion void
+*   No retorna nada ya que es una funcion void
 *****/
 
 bool elemento_comun(Persona* p1, Persona* p2){
     bool se_puede = false;
-    for (int u=0; u< p1->tamanio_tarjeta ; u++){
-        for (int g=0; g< p2->tamanio_tarjeta; g++){
-            if (p1->tarjeta[u] == p2->tarjeta[g]){
+    for (int u=0; u < p1->tamanio_tarjeta ; u++){
+        for (int g=0; g < p2->tamanio_tarjeta; g++){
+            if (p1 -> tarjeta[u] == p2 -> tarjeta[g]){
                 se_puede = true;
                 return se_puede;
             }}}
@@ -80,7 +83,7 @@ bool elemento_comun(Persona* p1, Persona* p2){
 * void intercambiarTarjeta
 ******
 * Resumen:
-* 	Intercambia las tarjetas (arreglo de enteros) de dos personas.
+*   Intercambia las tarjetas (arreglo de enteros) de dos personas.
 ******
 * Input:
 *   -Persona* p1; es un arreglo de struct en donde se guarda los datos de una persona 
@@ -88,63 +91,40 @@ bool elemento_comun(Persona* p1, Persona* p2){
 *   -int *aux; es un arreglo en donde se almacenara temporalmente la tarjeta de p1 (Ayuda a hacer el intercambio más facil)
 ******
 * Returns:
-* 	No retorna nada ya que es una funcion void
+*   No retorna nada ya que es una funcion void
 *****/
 
 void intercambiarTarjeta(Persona* p1, Persona* p2){  
+    int aux = p1 -> tamanio_tarjeta;
 
-    int* aux = new int[p1->tamanio_tarjeta];
-    int* aux2 = new int[p2->tamanio_tarjeta];
+    p1 -> tamanio_tarjeta = p2 -> tamanio_tarjeta;
+    p2 -> tamanio_tarjeta = aux;
 
-    for (int i=0; i<p1->tamanio_tarjeta; i++){
-        aux[i] = p1->tarjeta[i];
-    }
+    int *aux2 = p1 -> tarjeta;
 
-    for (int j=0; j<p2->tamanio_tarjeta; j++){
-        aux2[j] = p2->tarjeta[j];
-    }
-
-    delete[] p1->tarjeta;
-    delete[] p2->tarjeta;
-
-    for (int i=0; i<p2->tamanio_tarjeta; i++){
-        p1->tarjeta[i] = aux2[i];
-    }
-
-    for (int j=0; j<p1->tamanio_tarjeta; j++){
-        p2->tarjeta[j] = aux[j];
-    }
-
-
-    p1->tarjeta = aux2;
-    p2->tarjeta = aux;
-
-    int g = 0;
-    g = p1->tamanio_tarjeta;
-    p1->tamanio_tarjeta = p2->tamanio_tarjeta;
-    p2->tamanio_tarjeta = g; 
-
+    p1 -> tarjeta = p2 -> tarjeta;
+    p2 -> tarjeta = aux2;
 };
 /*---------------------------------------------------------------------------------------*/
 /*****
 * int puntaje
 ******
 * Resumen:
-* 	Calcula los puntos que tiene la persona, utilizando la tarjeta de enteros y su fecha
+*   Calcula los puntos que tiene la persona, utilizando la tarjeta de enteros y su fecha
 ******
 * Input:
 *   -int puntaje; Es el puntaje total que obtuvo la persona y que va a retornar la funcion
 *   -int suma; Es la suma que obtiene la persona en cada iteración de la suma
 ******
 * Returns:
-* 	Retornara el puntaje total que obtuvo la persona con su tarjeta de enteros y su fecha
+*   Retornara el puntaje total que obtuvo la persona con su tarjeta de enteros y su fecha
 *****/
 
 int puntaje(Persona* p1){
     int puntaje = 0, sum;
     //cout<<endl<<p1->nombre<<" y fecha "<<p1->fecha<<endl;
-    for (int z=0; z < (p1->tamanio_tarjeta); z++){
-        sum = (p1->tarjeta[z])* p1->fecha[z%10];
+    for (int z = 0; z < ( p1 -> tamanio_tarjeta); z++) {
+        sum = (p1 -> tarjeta[z]) * p1 -> fecha[z%10];
         puntaje += sum;
     }
 
@@ -157,7 +137,7 @@ int puntaje(Persona* p1){
 * Persona* unDia
 ******
 * Resumen:
-* 	Se encarga de verificar que todas las personas tengan una tarjeta de la suerte, ayudar a filtrar quienes quieren hacer el intercambio de tarjetas de la suerte y por último determina que persona sacó el mayor puntaje.
+*   Se encarga de verificar que todas las personas tengan una tarjeta de la suerte, ayudar a filtrar quienes quieren hacer el intercambio de tarjetas de la suerte y por último determina que persona sacó el mayor puntaje.
 ******
 * Input:
 *   -Persona* personas; es un arreglo de struct que guarda toda la informacion de las personas
@@ -169,65 +149,39 @@ int puntaje(Persona* p1){
 *   -int m; es un entero que guarda el largo del arreglo que se debe crear para la función comprarTarjeta
 ******
 * Returns:
-* 	Un arreglo de struct con los datos del ganador del dia
+*   Un arreglo de struct con los datos del ganador del dia
 *****/
+
 Persona* unDia(Persona* personas, int dia){ 
-
-    cout<<"Dia "<<dia<<endl;
-    Persona *ganadorDia= &personas[0];
-    int puntos= 0, puntosMax= -1;
-    int size=l; 
-    
-    if(dia==1){
-        for(int k=0; k<size; k++){
-            int m = personas[k].nombre.length();
-            personas[k].tamanio_tarjeta = m;
-            delete[] personas[k].tarjeta;
-            personas[k].tarjeta = comprarTarjeta(personas[k].nombre, dia, m );
-        }
+    for (int i = 0; i < numPersonas; i++) {
+        personas[i].tarjeta = comprarTarjeta(personas[i].nombre, dia, personas[i].tamanio_tarjeta);
     }
 
-
-    for(int i = 0; i<size; i++){
-        if(i!=0){
-            int m = personas[i].tamanio_tarjeta;
-            delete[] personas[i].tarjeta;
-            personas[i].tarjeta= comprarTarjeta(personas[i].nombre, dia, m );
-        }
-    }
-        //personas que quieren cambiar
-    for(int i=0; i<size; i++){
-        bool se_puede;
-        if(personas[i].quiere_intercambiar==true){
-            for(int j = 0; j<size; j++){
-                if((i != j) && personas[j].quiere_intercambiar == true){
-                    se_puede = elemento_comun(&personas[i], &personas[j]);
-                    if (se_puede){
-                        intercambiarTarjeta(&personas[i], &personas[j]);
-                        personas[i].quiere_intercambiar = false;
-                        j = size;
-                    }
-                }
+    for (int i = 0; i < numPersonas - 1;  i++) {
+        for (int j = i + 1; j < numPersonas; j++) {
+            if (personas[i].quiere_intercambiar == 1 && personas[j].quiere_intercambiar == 1 && elemento_comun(&personas[i], &personas[j])){
+                intercambiarTarjeta(&personas[i], &personas[j]);
             }
         }
     }
 
-    for ( int i=0; i<size; i++){
-        puntos= puntaje(&personas[i]);
-        if(puntos>puntosMax){
-            puntosMax=puntos;
-            ganadorDia= &personas[i];
+    int puntajeGanador = -1;
+    int posGanador;
+    for (int i = 0; i < numPersonas; i++) {
+        if (puntajeGanador < puntaje(&personas[i])){
+            puntajeGanador = puntaje(&personas[i]);
+            posGanador = i;
         }
     }
 
-    return ganadorDia;
+    return &personas[posGanador];
 }
 /*---------------------------------------------------------------------------------------*/
 /*****
 * void variosDias
 ******
 * Resumen:
-* 	Se dedica hacer repetir la funcion dia hasta que la cantidad de dias se lo indique, también imprime por pantalla los dato del ganador del dia
+*   Se dedica hacer repetir la funcion dia hasta que la cantidad de dias se lo indique, también imprime por pantalla los dato del ganador del dia
 ******
 * Input:
 *   -Persona* personas; es un arreglo de struct que guarda toda la informacion de las personas.
@@ -235,35 +189,20 @@ Persona* unDia(Persona* personas, int dia){
 *   -Persona *ganador; es un arreglo de struct que guarda la informacion de la persona que gano el dia.
 ******
 * Returns:
-* 	No retorna nada ya que es una funcion void.
+*   No retorna nada ya que es una funcion void.
 *****/
 
 void variosDias(Persona* personas,int cant_dias ){
     Persona *ganador;
-    int tam=0, z=0;
-    int puntajeGanador;
+    for (int i = 1; i <= cant_dias; i++){
+        cout << "Dia: " << i << endl;
 
-
-    for(int i=0; i<l; i++){
-        if(personas[i].quiere_intercambiar==true) tam++;
-    }
-    int arr[tam];
-
-    for(int i=0; i<l; i++){
-        if(personas[i].quiere_intercambiar==true){
-            arr[z]=i;
-            z++;
-        }
-    }
-
-    for(int i=1; i<=cant_dias; i++){
         ganador = unDia(personas, i);
-        puntajeGanador = puntaje(ganador);
 
-        cout<< ganador->nombre <<" "<< ganador->fecha <<" "<< puntajeGanador <<endl;
+        cout << ganador -> nombre << ' ' << ganador -> fecha << ' ' << puntaje(ganador) << endl;
 
-        for(int i=0; i<tam; i++){
-            personas[arr[i]].quiere_intercambiar= true;
+        for (int i = 0; i < numPersonas; i++){
+            delete[] personas[i].tarjeta;
         }
     }
 }
@@ -273,7 +212,7 @@ void variosDias(Persona* personas,int cant_dias ){
 *   void ListasDeLaSuerte
 ******
 * Resumen:
-* 	Es una función que pide los datos de la personas que quieren registrar, pide la cantidad de dias para así ayuda a ordenar esos datos para prepararlas a las futuras funciones y despues elimina todas la memorias dinamicas que se usaron. 
+*   Es una función que pide los datos de la personas que quieren registrar, pide la cantidad de dias para así ayuda a ordenar esos datos para prepararlas a las futuras funciones y despues elimina todas la memorias dinamicas que se usaron. 
 ******
 * Input:
 *   -int l; es un entero que indica la cantidad de datos de personas que hay
@@ -286,24 +225,24 @@ void variosDias(Persona* personas,int cant_dias ){
 *   -int j; es un entero que ayuda a indicar la posicion del dato de entrada de la persona.
 ******
 * Returns:
-* 	No retorna nada ya que es una funcion void.
+*   No retorna nada ya que es una funcion void.
 *****/
 
-void ListasDeLaSuerte(int l){
+void ListasDeLaSuerte(){
     Persona p, *personas;
     int cant_dias;
 
-    string *listaPersonas = new string[l];
+    string *listaPersonas = new string[numPersonas];
     
     cin.ignore();
-    for(int i=0; i<l; i++){
+    for(int i=0; i < numPersonas; i++){
         getline(cin, p.nombre);
         listaPersonas[i]+=p.nombre;
     }
     cin>>cant_dias;
 
-    personas = new Persona[l];
-    for(int i=0; i<l; i++){
+    personas = new Persona[numPersonas];
+    for(int i=0; i < numPersonas; i++){
         string dato=listaPersonas[i];
         string nombre;
         int j=0;    
@@ -325,7 +264,7 @@ void ListasDeLaSuerte(int l){
         for (int v=0; v < large ;v++){
             personas[i].fecha[v] = f[v];
         };
-        //personas[i].fecha[10]=' ';
+
         j++;
     
         if(dato[j] == '0'){
@@ -335,34 +274,30 @@ void ListasDeLaSuerte(int l){
         }
 
         personas[i].nombre=nombre;
-
-        personas[i].tamanio_tarjeta=nombre.length();
-        personas[i].tarjeta=NULL;
+        personas[i].tamanio_tarjeta = sizeof(personas[i].nombre);
+        //personas[i].tarjeta=NULL;
     }
 
     variosDias(personas, cant_dias);
-
-    for (int i = 0; i < l; i++){
-        delete[] personas[i].tarjeta;
-    }
-
+    
     delete[] listaPersonas;
     delete[] personas;
 };
 /*---------------------------------------------------------------------------------------*/
 /***********
 * Resumen:
-* 	Pedir el dato de cuantas personas quiere registrar y ese dato lo va a recibir la funcion ListasDeLaSuerte
+*   Pedir el dato de cuantas personas quiere registrar y ese dato lo va a recibir la funcion ListasDeLaSuerte
 ******
 * Input:
 *   -int l; es un entero de variable global que indica la cantidad de datos de personas que hay para entregarselo a la funcion ListasDeLaSuerte
 ******
 * Returns:
-* 	int, retorna 0 si se ejecuta de forma exitosa.
+*   int, retorna 0 si se ejecuta de forma exitosa.
 *****/
+
 int main(){
-    cin>>l;
-    ListasDeLaSuerte(l);
+    cin>>numPersonas;
+    ListasDeLaSuerte();
 
     return 0;
 }
