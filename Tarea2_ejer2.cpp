e9983d296dacf4f5afda19f4d1dfb20234bbf719
AlvaroTuno <116527050+AlvaroTuno@users.noreply.github.com>
2023-05-26 18:40:53 -0400

    Update Tarea2_ejer2.cpp
    El ABB sirve para almacenar los postes encendidos, y ya termine de crear las funciones Encendido y PararPrograma. Solo queda por hacer la función CuantosEncendidos

--- Tarea2_ejer2.cpp
+++ Tarea2_ejer2.cpp
@@ -5,46 +5,71 @@
 #include<cstring>
 #include "TDA de ABB.hpp"
 
-
-
 using namespace std;
 
-void Encender(int i){
+class AvenidaLuces{
+	private:
+		int PostesEncendidos;
+		tABB postes;	//En el ABB vamos a guardar todos los postes que están encendidos
+	public:
+		AvenidaLuces(){
+			int PostesEncendidos=0;
+		}
+		~AvenidaLuces(){
+			int PostesEncendidos=NULL;
+		}
+
+		void Encender(int i);
+		void CuantosEncender(int i);
+		void PararPrograma();
+
+};
+
+void AvenidaLuces::Encender(int i){
+	//Enciende el poste numero i, si el poste ya esta encendido entonces no ocurre nada
+	if(postes.find(i) == false){
+		postes.insert(i);
+		PostesEncendidos++;
+	}
 
 }
 
-void CuantosEncender(int i){
+void AvenidaLuces::CuantosEncender(int i){
+
 
 }
 
-void PararPrograma(){
-
+void AvenidaLuces::PararPrograma(){
+	cout<<PostesEncendidos<<endl;
 }
+
 
 void LucesporArreglar(){
 	bool flag=true;
+
 	while(flag){
+		AvenidaLuces principal;
 		string entrada, orden;
-		int espacio, pos, aux;
+		int espacio, pos;
 
 		getline(cin, entrada);
 
 		if(entrada == "PARAR_PROGRAMA"){
 			flag=false;
-			PararPrograma();
+			principal.PararPrograma();
+		}else{
+			espacio= entrada.find(' ');
+			orden= entrada.substr(0,espacio);
+			pos= stoi(entrada.substr(espacio+1, entrada.length()));
+			//cout<<espacio<<'\n'<<orden<<'\n'<<pos<<endl;	
 		}
-
-		espacio= entrada.find(' ');
-		orden= entrada.substr(0,espacio);
-		pos= stoi(entrada.substr(espacio+1, entrada.length()));
-		//cout<<espacio<<'\n'<<orden<<'\n'<<pos<<endl;
 		
 		if(orden == "ENCENDER"){
-			Encender(pos);
+			principal.Encender(pos);
 		}
 		
 		if(orden == "CUANTOS_ENCENDER"){
-			CuantosEncender(pos);
+			principal.CuantosEncender(pos);
 		}
 	}
 }
