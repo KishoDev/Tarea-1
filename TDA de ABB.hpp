b07e692eb37e6b923a7f1970baf35b387be1fb0f
AlvaroTuno <116527050+AlvaroTuno@users.noreply.github.com>
2023-05-30 14:52:02 -0400

    Update TDA de ABB.hpp
    Actualizado para su formato de entrega (Falta comentar)

--- TDA de ABB.hpp
+++ TDA de ABB.hpp
@@ -1,7 +1,7 @@
 //TDA de ABB
-
 #include<iostream>
 #include<string>
+#include<cstring>
 
 using namespace std;
 
@@ -24,9 +24,8 @@ class tABB{
 		void mostrarArbolHelp(tNodoArbolBin *nodo, int cont);
 
 	public:
-		//Construtor y destructor
 		tABB(){
-			raiz=nullptr;
+			raiz=NULL;
 			nElems=0;
 		}
 		~tABB(){
@@ -38,18 +37,17 @@ class tABB{
 		int lower_bound(tipoElem item);
 		void clear();
 		void mostrarArbol();
-		
-		
 };
 //-----------------------------------------------------------------------
 void tABB::clear(){
+
 	clearHelp(raiz);
-	raiz=nullptr;
+	raiz=NULL;
 	nElems=0;
 }
 
 void tABB::clearHelp(tNodoArbolBin* nodo) {
-    if (nodo != nullptr) {
+	if (nodo != NULL) {
         clearHelp(nodo->izq);
         clearHelp(nodo->der);
         delete nodo;
@@ -57,12 +55,11 @@ void tABB::clearHelp(tNodoArbolBin* nodo) {
 }
 
 void tABB::insert(tipoElem item){
-	if(raiz==nullptr){
-		//cout<<"Se creo una raiz"<<endl;
+	if(raiz==NULL){
 		raiz = new tNodoArbolBin;
 		raiz->info= item;
-		raiz->der= nullptr;
-		raiz->izq= nullptr;
+		raiz->der= NULL;
+		raiz->izq= NULL;
 
 	}else{
 		insertHelp(raiz, item);
@@ -73,44 +70,28 @@ void tABB::insert(tipoElem item){
 
 void tABB::insertHelp(tNodoArbolBin *nodo, tipoElem item){
 	if(item > nodo->info){
-		if(nodo->der == nullptr){
-			//cout<<"Se inserto en la derecha"<<endl;
+		if(nodo->der == NULL){
 			nodo->der = new tNodoArbolBin;
 			nodo->der->info= item;
-			nodo->der->der= nullptr;
-			nodo->der->izq= nullptr;
+			nodo->der->der= NULL;
+			nodo->der->izq= NULL;
 		}else{
 			insertHelp(nodo->der, item);
 		}
 	}
 
 	if(item< nodo->info){
-		if(nodo->izq == nullptr){
-			//cout<<"Se inserto en la izquierda"<<endl;
+		if(nodo->izq == NULL){
             nodo->izq = new tNodoArbolBin;
             nodo->izq->info = item;
-            nodo->izq->der = nullptr;
-            nodo->izq->izq = nullptr;
+            nodo->izq->der = NULL;
+            nodo->izq->izq = NULL;
 		}else{
 			insertHelp(nodo->izq, item);
 		}
 	}
 }
 
-/*bool tABB::find(tipoElem item){
-	if(raiz== nullptr)	return false;
-	return findHelp(raiz, item);
-}
-
-bool tABB::findHelp(tNodoArbolBin *nodo, tipoElem item){
-	if(nodo->info == item) return true;
-
-	if(item > nodo->info && nodo->der != nullptr) findHelp(nodo->der, item);
-
-	if(item < nodo->info && nodo->izq != nullptr) findHelp(nodo->izq, item);
-
-	return false;
-}*/
 bool tABB::find(tipoElem item){
 	return findHelp(raiz, item);
 }
@@ -128,28 +109,17 @@ bool tABB::findHelp(tNodoArbolBin *nodo, tipoElem item){
 }
 
 int tABB::lower_bound(tipoElem item){
-	if(raiz== nullptr)	return 0;
+	if(raiz== NULL)	return 0;
 	return lower_boundHelp(raiz, item);
 }
 
-/*
 int tABB::lower_boundHelp(tNodoArbolBin *nodo, tipoElem item){
 	if(nodo->info <= item) return nodo->info;
-	if(nodo->izq != nullptr) lower_boundHelp(nodo->izq, item);
+	if(nodo->izq != NULL) lower_boundHelp(nodo->izq, item);
 
 	return 0;
-}*/
-
-int tABB::lower_boundHelp(tNodoArbolBin *nodo, tipoElem item){
-	if(nodo->izq != nullptr){
-		return lower_boundHelp(nodo->izq, item);
-	}else{
-		return nodo->info;
-	}
 }
 
-
-//función para mostrar arbol completo (de costado)
 void tABB::mostrarArbol(){
 	mostrarArbolHelp(raiz, 0);
 }
@@ -166,20 +136,3 @@ void tABB::mostrarArbolHelp(tNodoArbolBin *nodo, int cont){
 		mostrarArbolHelp(nodo->izq, cont+1);
 	}
 }
-
-
-//------------testeo--------------------------------
-/*
-int main(){
-	tABB arbol;
-	
-	cout<<arbol.find(17)<<endl;
-	cout<<arbol.lower_bound(5)<<endl;
-
-	arbol.clear();
-	//g++ -g 'TDA de ABB.cpp' -o alo
-	//valgrind --leak-check=yes ./'TDA de ABB.cpp'
-	
-	return 0;
-};
-*/
